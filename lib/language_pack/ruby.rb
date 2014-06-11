ce40985cc44d135826f341a7b95709b596d3e6c0
Youssef Shahin <yshahin@gmail.com>
2014-06-11 12:01:04 +0300

    Remove parallel bundle jobs

--- lib/language_pack/ruby.rb
+++ lib/language_pack/ruby.rb
@@ -476,7 +476,6 @@ WARNING
         bundle_without = env("BUNDLE_WITHOUT") || "development:test"
         bundle_bin     = "bundle"
         bundle_command = "#{bundle_bin} install --without #{bundle_without} --path vendor/bundle --binstubs #{bundler_binstubs_path}"
-        bundle_command << " -j4"
 
         if bundler.windows_gemfile_lock?
           warn(<<WARNING, inline: true)
